c16c7fbd7fc346d336c3b3e84efd98baf05f41f9
ƬHE ЯAW ☣ <raw@dopehosting.net>
2018-10-21 18:42:02 +0200

    Update full.php

--- json/full.php
+++ json/full.php
@@ -1,4 +1,19 @@
 <?php
 require '../../config.php';
+# TOTAL ADMINS
+$db->where ("is_admin", 1);
+$totalAdmins = $db->getValue ("users", "count(id)");
+# echo "TOTAL ADMINS : $totalAdmins";
+# END TOTAL ADMINS
 
+# TOTAL USERS
+$db->where ("is_admin", 0);
+$totalUsers = $db->getValue ("users", "count(id)");
+# echo "TOTAL USERS : $totalUsers";
+# END TOTAL USERS
+
+# TOTAL SERVERS
+$totalServers = $db->getValue ("servers", "count(id)");
+# echo "TOTAL SERVERS : $totalServers";
+# END TOTAL SERVERS
 ?>
